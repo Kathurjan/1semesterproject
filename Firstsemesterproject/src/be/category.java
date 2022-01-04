c0c85bc7dd74c9685063c70d33b8fa3116aa12cb
ChristianHesberg <chri59kx@easv365.dk>
2022-01-04 17:55:02 +0100

    Merge branch 'main' into Christian

---
+++ Firstsemesterproject/src/be/category.java
@@ -0,0 +1,26 @@
+package be;
+
+public class category {
+
+
+    private String categoryName;
+    private int id;
+
+    public category(String categoryName, int id){
+        this.categoryName = categoryName;
+        this.id = id;
+
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+    public int getId() {
+        return id;
+    }
+
+}

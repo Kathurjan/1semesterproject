9e8c01ce2c3066f3b864b8b83fa9985b2a0c2025
ChristianHesberg <chri59kx@easv365.dk>
2022-01-12 10:33:01 +0100

    ...

--- Firstsemesterproject/src/MovieCollection/gui/controller/MovieCollectionController.java
+++ Firstsemesterproject/src/MovieCollection/gui/controller/MovieCollectionController.java
@@ -9,8 +9,6 @@ import MovieCollection.gui.model.SearchModel;
 import MovieCollection.gui.model.TableViewMoviesModel;
 import MovieCollection.gui.view.*;
 import com.microsoft.sqlserver.jdbc.SQLServerException;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
